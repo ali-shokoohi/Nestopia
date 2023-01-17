2d034f020a9736d3b2f2679deab9488a728b4760
Ali Shokoohi <shokoohi.pub@gmail.com>
2023-01-17 19:51:27 +0330

    Using with RxJS library for reactive programming

--- src/user/controller/user/user.controller.ts
+++ src/user/controller/user/user.controller.ts
@@ -1,35 +1,151 @@
-import { Controller, Get, Post, Delete, Body, Param, Put } from "@nestjs/common";
+import { Controller, Get, Post, Delete, Body, Param, Put, HttpException, HttpStatus } from "@nestjs/common";
 import { UserService } from "../../service/user/user.service";
 import { UserDto } from "../../dto/user.dto/user.dto";
 import { DeleteResult, UpdateResult } from "typeorm";
+import * as RxJS from 'rxjs';
 
 @Controller("users")
 export class UserController {
 	constructor(private readonly userService: UserService) {}
 
 	@Post()
-	create(@Body() user: UserDto): Promise<UserDto> {
-		return this.userService.create(user);
+	create(@Body() user: UserDto): RxJS.Observable<UserDto> {
+		return RxJS.from(this.userService.create(user))
+		.pipe(
+			RxJS.catchError(error => 
+				RxJS.merge(
+					RxJS.of(error)
+					.pipe(
+						RxJS.filter(error => error instanceof HttpException),
+						RxJS.mergeMap(error => RxJS.throwError(error)),
+					),
+					RxJS.of(error)
+					.pipe(
+						RxJS.filter(error => !(error instanceof HttpException)),
+						RxJS.mergeMap(error =>
+							RxJS.throwError(() => new HttpException(
+								{
+             				     statusCode: '500',
+               					   message: 'Something Went Wrong',
+                  					error: 'Internal Server Erroreee'
+                					}, HttpStatus.INTERNAL_SERVER_ERROR)
+							))
+						)
+					)
+				)
+				);
 	}
 
 	@Get()
-	findAll(): Promise<UserDto[]> {
-		return this.userService.findAll();
+	findAll(): RxJS.Observable<UserDto[]>{
+		return RxJS.from(this.userService.findAll())
+		.pipe(
+			RxJS.catchError(error => 
+				RxJS.merge(
+					RxJS.of(error)
+					.pipe(
+						RxJS.filter(error => error instanceof HttpException),
+						RxJS.mergeMap(error => RxJS.throwError(error)),
+					),
+					RxJS.of(error)
+					.pipe(
+						RxJS.filter(error => !(error instanceof HttpException)),
+						RxJS.mergeMap(error =>
+							RxJS.throwError(() => new HttpException(
+								{
+             				     statusCode: '500',
+               					   message: 'Something Went Wrong',
+                  					error: 'Internal Server Erroreee'
+                					}, HttpStatus.INTERNAL_SERVER_ERROR)
+							))
+						)
+					)
+				)
+				);
 	}
 
 	@Get("/:id")
-	findOneByID(@Param("id") id: number): Promise<UserDto> {
-		return this.userService.findOneByID(id);
+	findOneByID(@Param("id") id: number): RxJS.Observable<UserDto> {
+		return RxJS.from(this.userService.findOneByID(id))
+		.pipe(
+			RxJS.catchError(error => 
+				RxJS.merge(
+					RxJS.of(error)
+					.pipe(
+						RxJS.filter(error => error instanceof HttpException),
+						RxJS.mergeMap(error => RxJS.throwError(error)),
+					),
+					RxJS.of(error)
+					.pipe(
+						RxJS.filter(error => !(error instanceof HttpException)),
+						RxJS.mergeMap(error =>
+							RxJS.throwError(() => new HttpException(
+								{
+             				     statusCode: '500',
+               					   message: 'Something Went Wrong',
+                  					error: 'Internal Server Erroreee'
+                					}, HttpStatus.INTERNAL_SERVER_ERROR)
+							))
+						)
+					)
+				)
+				);
 	}
 
 	@Put("/:id")
-	updateOneByID(@Param("id") id: number, @Body() user: UserDto): Promise<UpdateResult> {
-		return this.userService.updateOneByID(id, user);
+	updateOneByID(@Param("id") id: number, @Body() user: UserDto): RxJS.Observable<UpdateResult> {
+		return RxJS.from(this.userService.updateOneByID(id, user))
+		.pipe(
+			RxJS.catchError(error => 
+				RxJS.merge(
+					RxJS.of(error)
+					.pipe(
+						RxJS.filter(error => error instanceof HttpException),
+						RxJS.mergeMap(error => RxJS.throwError(error)),
+					),
+					RxJS.of(error)
+					.pipe(
+						RxJS.filter(error => !(error instanceof HttpException)),
+						RxJS.mergeMap(error =>
+							RxJS.throwError(() => new HttpException(
+								{
+             				     statusCode: '500',
+               					   message: 'Something Went Wrong',
+                  					error: 'Internal Server Erroreee'
+                					}, HttpStatus.INTERNAL_SERVER_ERROR)
+							))
+						)
+					)
+				)
+				);
 	}
 
 	@Delete("/:id")
-	deleteOneByID(@Param("id") id: number): Promise<DeleteResult> {
-		return this.userService.deleteOneByID(id);
+	deleteOneByID(@Param("id") id: number): RxJS.Observable<DeleteResult> {
+		return RxJS.from(this.userService.deleteOneByID(id))
+		.pipe(
+			RxJS.catchError(error => 
+				RxJS.merge(
+					RxJS.of(error)
+					.pipe(
+						RxJS.filter(error => error instanceof HttpException),
+						RxJS.mergeMap(error => RxJS.throwError(error)),
+					),
+					RxJS.of(error)
+					.pipe(
+						RxJS.filter(error => !(error instanceof HttpException)),
+						RxJS.mergeMap(error =>
+							RxJS.throwError(() => new HttpException(
+								{
+             				     statusCode: '500',
+               					   message: 'Something Went Wrong',
+                  					error: 'Internal Server Erroreee'
+                					}, HttpStatus.INTERNAL_SERVER_ERROR)
+							))
+						)
+					)
+				)
+				);
 	}
 
 }
--- src/user/service/user/user.service.ts
+++ src/user/service/user/user.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
+import * as RxJS from 'rxjs';
 import { UserEntity } from "src/user/entity/user.entity/user.entity";
 import { Repository, DeleteResult, UpdateResult } from "typeorm";
 import { UserDto } from "../../dto/user.dto/user.dto";
@@ -12,40 +13,217 @@ export class UserService {
 	){}
 
 	// A method for creating a user object into database
-	create(user: UserDto): Promise<UserDto> {
-		return this.userRepository.save(user);
+	create(user: UserDto): RxJS.Observable<UserDto> {
+		return RxJS.from(this.userRepository.save(user))
+		.pipe(
+			RxJS.tap({
+				    next: result => console.debug(`create user success: ${result}`),
+        			error: err => console.error(`create user failed:`, err)
+			}),
+			RxJS.catchError(_ => RxJS.throwError(() => new HttpException(
+				{
+					status: 500,
+					message: 'Something Went Wrong',
+          			error: 'Internal Server Error'
+				},
+				HttpStatus.INTERNAL_SERVER_ERROR
+			))),
+			RxJS.mergeMap(result => 
+				RxJS.merge(
+					RxJS.of(result)
+					.pipe(
+						RxJS.filter(queryResult => !queryResult),
+						RxJS.mergeMap(_ => RxJS.throwError(() => new HttpException (
+							{
+								status: 400,
+								message: "User Not Created",
+								error: "Bad Request"
+							},
+							HttpStatus.BAD_REQUEST
+						)))
+					),
+					RxJS.of(result)
+					.pipe(
+						RxJS.filter(queryResult => !!queryResult),
+						RxJS.identity
+					)
+				)
+			)
+		);
 	}
 
 	// A method for getting all users from database
-	findAll(): Promise<UserDto[]> {
-		return this.userRepository.find();
+	findAll(): RxJS.Observable<UserDto[]> {
+		return RxJS.from(this.userRepository.find())
+		.pipe(
+			RxJS.tap({
+				    next: result => console.debug(`findAll users success: ${result}`),
+        			error: err => console.error(`findAll users failed:`, err)
+			}),
+			RxJS.catchError(_ => RxJS.throwError(() => new HttpException(
+				{
+					status: 500,
+					message: 'Something Went Wrong',
+          			error: 'Internal Server Error'
+				},
+				HttpStatus.INTERNAL_SERVER_ERROR
+			))),
+			RxJS.mergeMap(result => 
+				RxJS.merge(
+					RxJS.of(result)
+					.pipe(
+						RxJS.filter(queryResult => !queryResult),
+						RxJS.mergeMap(_ => RxJS.throwError(() => new HttpException (
+							{
+								status: 404,
+								message: "Users Not Found",
+								error: "Not Found"
+							},
+							HttpStatus.NOT_FOUND
+						)))
+					),
+					RxJS.of(result)
+					.pipe(
+						RxJS.filter(queryResult => !!queryResult),
+						RxJS.identity
+					)
+				)
+			)
+		);
 	}
 
 	// A mothod for getting one user from database
-	findOneByID(id: number): Promise<UserDto> {
-		return this.userRepository.findOneBy(
+	findOneByID(id: number): RxJS.Observable<UserDto> {
+		return RxJS.from(this.userRepository.findOneBy(
 			{
 				id: id
-			}
-		)
+			}))
+		.pipe(
+			RxJS.tap({
+				    next: result => console.debug(`findOneByID user success: ${result}`),
+        			error: err => console.error(`findOneByID user failed:`, err)
+			}),
+			RxJS.catchError(_ => RxJS.throwError(() => new HttpException(
+				{
+					status: 500,
+					message: 'Something Went Wrong',
+          			error: 'Internal Server Error'
+				},
+				HttpStatus.INTERNAL_SERVER_ERROR
+			))),
+			RxJS.mergeMap(result => 
+				RxJS.merge(
+					RxJS.of(result)
+					.pipe(
+						RxJS.filter(queryResult => !queryResult),
+						RxJS.mergeMap(_ =>
+							RxJS.throwError(() => new HttpException (
+							{
+								status: 404,
+								message: "User Not Found",
+								error: "Not Found"
+							},
+							HttpStatus.NOT_FOUND
+						)))
+					),
+					RxJS.of(result)
+					.pipe(
+						RxJS.filter(queryResult => !!queryResult),
+						RxJS.identity
+					)
+				)
+			)
+		);
 	}
 
 	// A mothod for updating one user from database
-	updateOneByID(id: number, user: UserDto): Promise<UpdateResult> {
-		return this.userRepository.update(
+	updateOneByID(id: number, user: UserDto): RxJS.Observable<UpdateResult> {
+		return RxJS.from(this.userRepository.update(
 			{
 				id: id
 			},
 			user
-		)
+		))
+		.pipe(
+			RxJS.tap({
+				    next: result => console.debug(`findOneByID user success: ${result}`),
+        			error: err => console.error(`findOneByID user failed:`, err)
+			}),
+			RxJS.catchError(_ => RxJS.throwError(() => new HttpException(
+				{
+					status: 500,
+					message: 'Something Went Wrong',
+          			error: 'Internal Server Error'
+				},
+				HttpStatus.INTERNAL_SERVER_ERROR
+			))),
+			RxJS.mergeMap(result => 
+				RxJS.merge(
+					RxJS.of(result)
+					.pipe(
+						RxJS.filter(queryResult => !queryResult),
+						RxJS.mergeMap(_ =>
+							RxJS.throwError(() => new HttpException (
+							{
+								status: 404,
+								message: "User Not Found",
+								error: "Not Found"
+							},
+							HttpStatus.NOT_FOUND
+						)))
+					),
+					RxJS.of(result)
+					.pipe(
+						RxJS.filter(queryResult => !!queryResult),
+						RxJS.identity
+					)
+				)
+			)
+		);
 	}
 
 	// A mothod for deleting one user from database
-	deleteOneByID(id: number): Promise<DeleteResult> {
-		return this.userRepository.delete(
+	deleteOneByID(id: number): RxJS.Observable<DeleteResult> {
+		return RxJS.from(this.userRepository.delete(
 			{
 				id: id
 			}
-		)
+		))
+		.pipe(
+			RxJS.tap({
+				    next: result => console.debug(`findOneByID user success: ${result}`),
+        			error: err => console.error(`findOneByID user failed:`, err)
+			}),
+			RxJS.catchError(_ => RxJS.throwError(() => new HttpException(
+				{
+					status: 500,
+					message: 'Something Went Wrong',
+          			error: 'Internal Server Error'
+				},
+				HttpStatus.INTERNAL_SERVER_ERROR
+			))),
+			RxJS.mergeMap(result => 
+				RxJS.merge(
+					RxJS.of(result)
+					.pipe(
+						RxJS.filter(queryResult => !queryResult),
+						RxJS.mergeMap(_ =>
+							RxJS.throwError(() => new HttpException (
+							{
+								status: 404,
+								message: "User Not Found",
+								error: "Not Found"
+							},
+							HttpStatus.NOT_FOUND
+						)))
+					),
+					RxJS.of(result)
+					.pipe(
+						RxJS.filter(queryResult => !!queryResult),
+						RxJS.identity
+					)
+				)
+			)
+		);
 	}
 }
